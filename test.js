de18686552edb32307f139ae9166b058948e9b85
cloudWheels <44451128+cloudwheels@users.noreply.github.com>
2020-10-19 12:44:58 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
  $(document).ready(function () {
 
-var items = ['2-system','3-work','4-admin'];
+var items = ['2-system','3-work','4-admins'];
 var i;
 for (i = 0; i < items.length; i++) {
   $('#content').append('<a href="./rules.html#' + items[i] + '">'+items[i]+'</a><br>');
